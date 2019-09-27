2ae258cff3f83aba471c204f1b142d1c66f8d8be
nicholas.garvey <garveyness03@gmail.com>
2019-09-27 13:47:22 -0400

    Started JSON

--- app/src/main/java/com/ngarvey/cctrivia/QuizActivity.kt
+++ app/src/main/java/com/ngarvey/cctrivia/QuizActivity.kt
@@ -1,10 +1,16 @@
 package com.ngarvey.cctrivia
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.questions_layout.*
 
 class QuizActivity : AppCompatActivity() {
+
+    // Intent keys
+    val CATEGORY_KEY = "category"
+    val SCORE_KEY = "score"
+    val QUESTION_NUMBER_KEY = "question_number"
 
     val CATEGORY_0 = 0
     val CATEGORY_1 = 1
@@ -14,7 +20,10 @@ class QuizActivity : AppCompatActivity() {
     lateinit var currentCategory: String
 
     companion object {
-
+        fun getCategoryBundle(): Intent {
+            val intent: Intent
+            return intent
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {

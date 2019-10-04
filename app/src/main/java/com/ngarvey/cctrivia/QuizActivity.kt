6a7418db9b1ab96fbed9d31db7d410d434d3b7d2
garveyn <garveyness03@gmail.com>
2019-10-03 23:08:37 -0400

    more work on JSON (done two days ago)

--- app/src/main/java/com/ngarvey/cctrivia/QuizActivity.kt
+++ app/src/main/java/com/ngarvey/cctrivia/QuizActivity.kt
@@ -4,27 +4,25 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.questions_layout.*
+import java.io.File
 
 class QuizActivity : AppCompatActivity() {
+
+    val QUESTIONS_PATH = "./src/main/res/JSON"
 
     // Intent keys
     val CATEGORY_KEY = "category"
     val SCORE_KEY = "score"
     val QUESTION_NUMBER_KEY = "question_number"
 
-    val CATEGORY_0 = 0
-    val CATEGORY_1 = 1
-    val CATEGORY_2 = 2
-    val CATEGORY_3 = 3
+    val CATEGORY_0 = "buildings.json"
+    val CATEGORY_1 = "faculty.json"
+    val CATEGORY_2 = "lore.json"
 
     lateinit var currentCategory: String
+    lateinit var questions: List<Question>
 
-    companion object {
-        fun getCategoryBundle(): Intent {
-            val intent: Intent
-            return intent
-        }
-    }
+    companion object {}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,6 +40,10 @@ class QuizActivity : AppCompatActivity() {
         choice3.setOnClickListener {
             gradeQuestion(3)
         }
+
+        // Load questions
+        File("$QUESTIONS_PATH/$currentCategory").readText()
+
 
     }
 
